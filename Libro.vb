934bf39e03b2b2117e506c221eb9b50ca87eaa1f
GABINETE 4 <GABINETE 4@PC-GAB4-17>
2024-10-04 18:01:50 -0300

    Agregar archivos de proyecto.

---
+++ Libro.vb
@@ -0,0 +1,60 @@
+﻿Public Class Libro
+    Private Shared NewId As Integer = 1000
+    Private id As Integer
+    Private titulo As String
+    Private autor As String
+    Private anio As Integer
+    Private paginas As Integer
+
+
+    Public Property GetTitulo As String
+        Get
+            Return titulo
+        End Get
+        Set(value As String)
+            titulo = value
+        End Set
+    End Property
+
+    Public Property GetAutor As String
+        Get
+            Return autor
+        End Get
+        Set(value As String)
+            autor = value
+        End Set
+    End Property
+
+    Public Property GetAnio As Integer
+        Get
+            Return anio
+        End Get
+        Set(value As Integer)
+            anio = value
+        End Set
+    End Property
+
+    Public Property GetPaginas As Integer
+        Get
+            Return paginas
+        End Get
+        Set(value As Integer)
+            If value > 0 Then paginas = value Else paginas = 1
+        End Set
+    End Property
+
+    Public Sub New(titulo_ As String, autor_ As String, anio_ As Integer, paginas_ As Integer)
+        id = NewId
+        Me.titulo = titulo_
+        Me.autor = autor_
+        Me.anio = anio_
+        Me.paginas = paginas_
+        id += 10
+    End Sub
+
+    Public Function Datos() As String
+        Datos = $"ID: {id}, titulo: {GetTitulo}, autor: {GetAutor}, Año: {GetAnio}, Paginas: {GetPaginas}"
+        Return Datos
+    End Function
+
+End Class
---
+++ Module1.vb
@@ -0,0 +1,102 @@
+﻿Module Module1
+    Dim libro As Libro
+
+    Dim titulo As String
+    Dim autor As String
+    Dim anio As Integer
+    Dim paginas As Integer
+
+    Dim libros(-1) As Libro
+    Sub Main()
+        Dim opcion As Integer
+
+        Do
+            Console.WriteLine("Menú:")
+            Console.WriteLine("1 - Alta libro")
+            Console.WriteLine("2 - Modificar libro")
+            Console.WriteLine("0 - Salir")
+
+            Console.Write("Elija una opción: ")
+            opcion = Convert.ToInt32(Console.ReadLine())
+
+            Select Case opcion
+                Case 1
+                    CrearLibro()
+                    Console.WriteLine($"{libro.Datos()}")
+
+                    ReDim Preserve libros(libros.Length)
+                    libros(libros.Length - 1) = New Libro(titulo, autor, anio, paginas)
+
+                    For i As Integer = 0 To (libros.Length - 1)
+                        Console.WriteLine(libros(i).Datos())
+                    Next
+                    Console.ReadKey()
+                    Console.Clear()
+                Case 2
+                    ModificarLibro()
+                    Console.WriteLine($"{libro.Datos()}")
+
+                Case 0
+                    Console.WriteLine("Saliendo...")
+                Case Else
+                    Console.WriteLine("Opción no válida, intente de nuevo.")
+            End Select
+            Console.WriteLine()
+        Loop While opcion <> 0
+    End Sub
+    Public Sub CrearLibro()
+        Console.WriteLine("")
+
+        Console.Write("Titulo: ")
+        libros(libros.Length - 1).GetTitulo = Console.ReadLine()
+
+        Console.WriteLine("")
+
+        Console.Write("Autor: ")
+        libros(libros.Length - 1).GetAutor = Console.ReadLine()
+
+        Console.WriteLine("")
+
+        Console.Write("Año de lanzamiento: ")
+        libros(libros.Length - 1).GetAnio = Integer.Parse(Console.ReadLine())
+
+        Console.WriteLine("")
+
+        Console.Write("Cantidad de paginas ")
+        libros(libros.Length - 1).GetPaginas = Integer.Parse(Console.ReadLine())
+    End Sub
+
+    Public Sub ModificarLibro()
+        For i As Integer = 0 To (libros.Length - 1)
+            Console.WriteLine(libros(i).Datos())
+        Next
+
+
+        Console.WriteLine("Ingrese libro a modificar")
+        Dim opcion As Integer = Integer.Parse(Console.ReadLine())
+        Console.Write("")
+
+
+        Console.Write("Titulo: ")
+        libros(opcion.Length - 1).GetTitulo = Console.ReadLine()
+
+        Console.WriteLine("")
+
+        Console.Write("Autor: ")
+        libros(opcion.Length - 1).GetAutor = Console.ReadLine()
+
+        Console.WriteLine("")
+
+        Console.Write("Año de lanzamiento: ")
+        libros(opcion.Length - 1).GetAnio = Integer.Parse(Console.ReadLine())
+
+        Console.WriteLine("")
+
+        Console.Write("Cantidad de paginas ")
+        libros(opcion - 1).GetPaginas = Integer.Parse(Console.ReadLine())
+
+        Console.ReadKey()
+        Console.Clear()
+    End Sub
+
+End Module
